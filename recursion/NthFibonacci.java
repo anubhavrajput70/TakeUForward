1d3e04916408ffb6e0bb4c0c811620473e7c71c5
Anubhav Rajput <anubhavrajput193@gmail.com>
2022-08-22 17:18:20 +0530

    Nth fibonacci number

---
+++ recursion/NthFibonacci.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class NthFibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+		System.out.println(fib(n));
+	}
+	static int fib(int n)
+	{
+		if(n<=1)
+			return n;
+		int first=fib(n-1);
+		int second=fib(n-2);
+		return first+second;
+	}
+
+}
