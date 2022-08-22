5b9c29a8a92f5b26bda79733700ead799df94712
Anubhav Rajput <anubhavrajput193@gmail.com>
2022-08-23 00:44:15 +0530

    String subSequence

---
+++ recursion/StirngSubSequence.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class StirngSubSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="abc";
+		int n=s.length();
+		sub(0,"",s,n);
+				
+	}
+	static void sub(int i,String a,String s,int n)
+	{
+		if(i>=n)
+		{
+			System.out.println(a);
+			return;
+		}
+		sub(i+1,a,s,n);
+		a=a+s.charAt(i);
+		sub(i+1,a,s,n);
+		
+		
+		
+		
+	}
+
+}
